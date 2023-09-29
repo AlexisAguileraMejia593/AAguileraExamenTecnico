76212e1957545918c5e3dc3c6df78ffc74312fdc
alexi <alexi@ALEXISAM99>
2023-09-29 00:48:54 -0600

    Agregar archivos de proyecto.

---
+++ BL/Disco.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Disco
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.MixUpEntities context = new DL.MixUpEntities())
+                {
+                    var query = context.DiscoGetAll().ToList();
+                    result.Objects = new List<object>();
+
+                    if (query != null)
+                    {
+
+                        foreach (var registro in query)
+                        {
+                            ML.Disco disco = new ML.Disco();
+                            disco.IdDisco = registro.IdDisco;
+                            disco.Titulo = registro.Titulo;
+                            disco.Artista = registro.Artista;
+                            disco.GeneroMusical = registro.GeneroMusical;
+                            disco.Duracion = registro.Duracion;
+                            disco.Año = registro.Año;
+                            disco.Distribuidora = registro.Distribuidora;
+                            disco.Ventas = registro.Ventas.Value;
+                            disco.Disponibilidad = registro.Disponibilidad.Value;
+
+                            result.Objects.Add(disco);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se encontraron los registros";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result Add(ML.Disco disco)
+        {
+                ML.Result result = new ML.Result();
+                try
+                {
+                    //modificacion de prueba
+                    using (DL.MixUpEntities context = new DL.MixUpEntities())
+                    {
+                        var query = context.DiscoAdd(
+                                                       disco.Titulo, 
+                                                       disco.Artista, 
+                                                       disco.GeneroMusical, 
+                                                       disco.Duracion,
+                                                       disco.Año, 
+                                                       disco.Distribuidora,
+                                                       disco.Ventas,
+                                                       disco.Disponibilidad);
+                        if (query >= 1)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    result.ErrorMessage = ex.Message;
+                }
+                return result;
+        }
+        public static ML.Result GetById(int IdDisco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.MixUpEntities context = new DL.MixUpEntities())
+                {
+                    var registro = context.DiscoGetById(IdDisco).FirstOrDefault();
+                    if (registro != null)
+                    {
+
+                        ML.Disco disco = new ML.Disco();
+                        disco.IdDisco = registro.IdDisco;
+                        disco.Titulo = registro.Titulo;
+                        disco.Artista = registro.Artista;
+                        disco.GeneroMusical = registro.GeneroMusical;
+                        disco.Duracion = registro.Duracion;
+                        disco.Año = registro.Año;
+                        disco.Distribuidora = registro.Distribuidora;
+                        disco.Ventas = registro.Ventas.Value;
+                        disco.Disponibilidad = registro.Disponibilidad.Value;
+                        result.Object = disco;
+
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result Update(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.MixUpEntities context = new DL.MixUpEntities())
+                {
+                    var query = context.DiscoUpdate(
+                        disco.IdDisco,
+                        disco.Titulo,
+                        disco.Artista,
+                        disco.GeneroMusical,
+                        disco.Duracion,
+                        disco.Año,
+                        disco.Distribuidora,
+                        disco.Ventas,
+                        disco.Disponibilidad
+                        );
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se actualizó el status de la credencial";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result Delete(int IdDisco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.MixUpEntities context = new DL.MixUpEntities())
+                {
+                    var registro = context.DiscoDelete(IdDisco);
+                    if (registro >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No rows were affected by the delete operation.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/DiscoController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class DiscoController : Controller
+    {
+        // GET: Disco
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Disco disco = new ML.Disco();
+            ML.Result result = BL.Disco.GetAll();
+            disco.Discos = result.Objects;
+            return View(disco);
+        }
+        [HttpGet]
+        public ActionResult Form(int? IdDisco)
+        {
+            ML.Disco disco = new ML.Disco();
+            if(IdDisco != null)
+            {
+                ML.Result result = BL.Disco.GetById(IdDisco.Value);
+                if (result.Correct)
+                {
+                    //UNBOXING
+                    disco = (ML.Disco)result.Object;
+                }
+            }
+            else
+            {
+
+            }
+            return View(disco);
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Disco disco)
+        {
+            if (disco.IdDisco == 0) //ADD
+            {
+                ML.Result result = BL.Disco.Add(disco);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "Se ha completado el registro";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Error" + result.ErrorMessage;
+
+                }
+            }
+            else //UPDATE
+            {
+                ML.Result result = BL.Disco.Update(disco);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "Se ha completado la actualizacion";
+
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Error" + result.ErrorMessage;
+                }
+            }
+            return PartialView("Modal");
+        }
+        public ActionResult Delete(int IdDisco)
+        {
+            ML.Disco disco = new ML.Disco();
+            ML.Result result = BL.Disco.Delete(IdDisco);
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "Se ha eliminado exitosamente!!!";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Error" + result.ErrorMessage;
+            }
+            return PartialView("Modal");
+        }
+    }
+}
